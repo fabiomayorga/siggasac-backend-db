f80d499234a9d3ef4ce3548ff8c4d5defa4c1a55
Arith Alexis Villalba Bravo <arithalexis@gmail.com>
2019-12-09 12:07:00 -0500

    export entities

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { DatabaseProvider } from './Database';
 
+export { Permission } from './entities/Permission';
+export { Profile } from './entities/Profile';
 export { User } from './entities/User';
