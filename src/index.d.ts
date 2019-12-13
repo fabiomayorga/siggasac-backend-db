1b39b762ba57ec06384b178c9d50d69e86454eb9
Arith Alexis Villalba Bravo <arithalexis@gmail.com>
2019-12-13 11:43:39 -0500

    add new entity

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,6 @@
-export { DatabaseProvider } from './Database';
+export { DatabaseProvider } from './DatabaseProvider';
 
+export { School } from './entities/School';
 export { Permission } from './entities/Permission';
 export { Profile } from './entities/Profile';
 export { User } from './entities/User';
