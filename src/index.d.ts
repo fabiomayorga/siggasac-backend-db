126831c5cca73875bbeb6c2869a86414546f9bd6
Arith Alexis Villalba Bravo <arithalexis@gmail.com>
2019-12-15 15:16:30 -0500

    added entities to index

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,8 @@
 export { DatabaseProvider } from './DatabaseProvider';
 
 export { Bank } from './entities/Bank';
+export { Country } from './entities/Country';
+export { Department } from './entities/Department';
 export { DocumentType } from './entities/DocumentType';
 export { Permission } from './entities/Permission';
 export { Profile } from './entities/Profile';
@@ -8,6 +10,7 @@ export { Revenue } from './entities/Revenue';
 export { School } from './entities/School';
 export { ThirdParty } from './entities/ThirdParty';
 export { ThirdPartyType } from './entities/ThirdPartyType';
+export { Town } from './entities/Town';
 export { TypePerson } from './entities/TypePerson';
 export { User } from './entities/User';
 export { Voucher } from './entities/Voucher';
