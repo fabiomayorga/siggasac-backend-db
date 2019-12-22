062081a3e78d4b6e3d1c40cafd59835440b5458e
aavillalbab <arithalexis@gmail.com>
2019-12-22 12:04:50 -0500

    new features are added to the index

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,19 +1,25 @@
 export { DatabaseProvider } from './DatabaseProvider';
 
+export { AccountType } from './entities/AccountType';
 export { Bank } from './entities/Bank';
+export { Campus } from './entities/Campus';
 export { Country } from './entities/Country';
 export { Department } from './entities/Department';
 export { DocumentType } from './entities/DocumentType';
 export { Menu } from './entities/Menu';
 export { MenuPermissionProfile } from './entities/MenuPermissionProfile';
+export { NatureDocument } from './entities/NatureDocument';
+export { PeopleType } from './entities/PeopleType';
 export { Permission } from './entities/Permission';
 export { Profile } from './entities/Profile';
+export { Project } from './entities/Project';
+export { Regime } from './entities/Regime';
 export { Revenue } from './entities/Revenue';
 export { School } from './entities/School';
 export { SchoolProfileUser } from './entities/SchoolProfileUser';
 export { ThirdParty } from './entities/ThirdParty';
+export { ThirdPartyAccounts } from './entities/ThirdPartyAccounts';
 export { ThirdPartyType } from './entities/ThirdPartyType';
 export { Town } from './entities/Town';
-export { PeopleType } from './entities/PeopleType';
 export { User } from './entities/User';
 export { Voucher } from './entities/Voucher';
